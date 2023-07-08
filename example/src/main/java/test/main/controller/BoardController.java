11d204bb308467cb7c8c6dcab03d85abed29d575
이하린 <102509224+hlleee@users.noreply.github.com>
2023-07-09 03:09:10 +0900

    공부중

--- example/src/main/java/test/main/controller/BoardController.java
+++ example/src/main/java/test/main/controller/BoardController.java
@@ -1,5 +1,9 @@
 package test.main.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +17,21 @@ import test.main.service.BoardService;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-	private final BoardService boardService;
 	
-	 @PostMapping("/writedo")	//delete,insert,update일 때 postMapping ,,, select는 GetMapping
-	    public String writedo(Board board, Model model) {
-	        boardService.write(board);
-	        
-	    }
+//	private final BoardService boardService;
+//	
+//	 @PostMapping("/writedo")	//delete,insert,update일 때 postMapping ,,, select는 GetMapping
+//	    public String writedo(Board board, Model model) {
+//	        boardService.write(board);
+//	        
+//	    }
+	 
+	 @GetMapping("/list")
+	 public String list(Model model,
+			 			@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageble,
+			 			String searchKeyword
+	) {
+		 Page<Board> list = null;
+	 }
 
 }
