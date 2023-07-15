aafbd9f80b84d0cc67ed42ec911cbc9a3b2e1b05
이하린 <102509224+hlleee@users.noreply.github.com>
2023-07-16 02:43:29 +0900

    7/15

--- example/src/main/java/test/main/controller/BoardController.java
+++ example/src/main/java/test/main/controller/BoardController.java
@@ -7,12 +7,18 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import test.main.entity.Board;
 import test.main.service.BoardService;
+import org.springframework.ui.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -40,21 +46,39 @@ public class BoardController {
 		 
 		 //searchKeyword가 없으면 select * from Board
 		 if (searchKeyword == null) {
-			 list = boardService.list(pageable); 
+			 list = boardService.list(Pageable); 
 			 //->BoardService 클래스 list 함수 코딩 필요.
 		 } else {
 			 //select * from Board where title=searchKeyword
 			 list = boardService.searchList(searchKeyword, pageable)
 			//->BoardService 클래스 searchList 함수 코딩 필요
 		 }
+		 int nowPage = list.getPageable().getPageNumber() + 1;//현재 페이지
+		 int startPage = Math.max(nowPage -4, 1);	//시작 페이지
+		 int endPage = Math.min(nowPage + 5, list.getTotalPages());	//끝 페이지
+		 
+		 model.addAttribute("list", list);
+		 model.addAttribute("nowPage", nowPage);
+		 model.addAttribute("startPage", startPage);
+		 model.addAttribute("endPage", endPage);
+		 
+		 return "board/list";
 	 }
-	 int nowPage = list.getPageable().getPageNumber() + 1;
-	 int startPage = Math.max(nowPage -4, 1);
-	 int endPage = Math.min(nowPage + 5, list.getTotalPages());
 	 
-	 model.addAttribute("list",list);
+	 @GetMapping("/view/{id}") //url에 view/id 번호 입력 혹은 a태그로 view/{id} 호출
+	 public String view(Model model, @PathVariable("id") Long id) {
+		//@PathVariablesms {변수} 안에 괄호를 열어 URL에 사용될 변수명으로 사용. 
+		 
+		 model.addAttribute("board", boardService.view(id));
+	 }
+	 
 }
 
+
+//model에 데이터를 담을 때 addAttribute() 메소드를 사용함.
+//사용 방법 2가지
+//#1 Model addAttribute(String name, Object value)
+//-> value 객체를 name 이름으로 추가한다. 뷰 코드에서는 name으로 지정한 이름을 통해서 value를 사용한다.
 
 
 
