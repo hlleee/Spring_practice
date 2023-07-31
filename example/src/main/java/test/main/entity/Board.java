0fecdc8d218d9a1daca01a31d9169efe00d42960
이하린 <102509224+hlleee@users.noreply.github.com>
2023-08-01 00:02:01 +0900

    7/31

--- example/src/main/java/test/main/entity/Board.java
+++ example/src/main/java/test/main/entity/Board.java
@@ -1,10 +1,10 @@
 package test.main.entity;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
